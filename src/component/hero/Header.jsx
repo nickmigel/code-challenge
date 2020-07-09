ca764c6638cd167bcd9e461d563c4376c962c82a
nick migel <nickmigel2@gmail.com>
2020-07-08 22:35:30 -0400

    found a work around for the circle divs

--- src/component/hero/Header.jsx
+++ src/component/hero/Header.jsx
@@ -12,32 +12,38 @@ const Header = () => {
                 <div id='hero-div'>
                     <img id='hero-img' src="/images/00.png" alt="chicken sandwich with lettuce tomatoe onions cheese pickles and ketchup" />
                 </div>
-            </div>
+            </div >
             <div id='miniMenu'>
                 <div className='menuCard'>
-                    <img src='/images/kisspng-doner-kebab-kapsalon-pizza-turkish-cuisine-kebab-5ac9869a962f36.7779223015231566346152.png' alt='sandwich with roast beef lettuce cheese tomatoes sour kraut and bacon' />
+                    <div className='circle one'>
+                        <img src='/images/kisspng-doner-kebab-kapsalon-pizza-turkish-cuisine-kebab-5ac9869a962f36.7779223015231566346152.png' alt='sandwich with roast beef lettuce cheese tomatoes sour kraut and bacon' />
+                    </div>
                     <div>
                         <h5>Ham Burger</h5>
                         <p>$10.30</p>
                     </div>
                 </div>
                 <div className='menuCard'>
-                    <img src='/images/kisspng-submarine-sandwich-barbecue-chicken-firehouse-subs-sub-sandwich-5b165469257050.2481338715281900571534.png'
-                        alt='firehouse sub sandwich' />
+                    <div className='circle two'>
+                        <img src='/images/kisspng-submarine-sandwich-barbecue-chicken-firehouse-subs-sub-sandwich-5b165469257050.2481338715281900571534.png'
+                            alt='firehouse sub sandwich' />
+                    </div>
                     <div>
                         <h5>Sub Sandwich</h5>
                         <p>$10.30</p>
                     </div>
                 </div>
                 <div className='menuCard'>
-                    <img src='/images/kisspng-chicago-style-hot-dog-hamburger-barbecue-bxe1nh-mx-hot-dog-5a735972beccb3.3331627015175089787815.png' alt='hot dog with mustard relish and pickles' />
+                    <div className='circle'>
+                        <img src='/images/kisspng-chicago-style-hot-dog-hamburger-barbecue-bxe1nh-mx-hot-dog-5a735972beccb3.3331627015175089787815.png' alt='hot dog with mustard relish and pickles' />
+                    </div>
                     <div>
                         <h5>Hot Dog</h5>
                         <p>$10.30</p>
                     </div>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
 
