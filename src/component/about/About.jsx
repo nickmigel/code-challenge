b251547856ce62cc0dea86cc3ddc12bc7a03e1cc
nick migel <nickmigel2@gmail.com>
2020-07-12 11:51:59 -0400

    think its alot closer to original

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -5,9 +5,11 @@ const About = () => {
         <div >
             <section id='about'>
                 <img src="/images/kisspng-hamburger-fast-food-burger-king-fizzy-drinks-frenc-huba-neuburg-tex-mex-burger-cocktails-5b21ebf2b03284.6482276015289497467217(1).png" alt="cheese burger falling into place with letuce onion tomato" />
-                <div>
+                <div id='about-text'>
                     <h2>Taste the best burger</h2>
-                    <p>we make every burger with hi-quality bun<br /> and fresh vegetables and meats so it<br /> taste real and authentic</p>
+                    <p>we make every burger with hi-quality bun</p>
+                    <p> and fresh vegetables and meats so it</p>
+                    <p>taste real and authentic</p>
                     <button>learn more</button>
                 </div>
             </section>
--- src/component/hero/Header.jsx
+++ src/component/hero/Header.jsx
@@ -6,46 +6,48 @@ const Header = () => {
             <div className='hero'>
                 <div>
                     <h1>TASTY BURGER</h1>
-                    <p>Grab the best tasty burger of the town we make the best<br /> burger for you</p>
+                    <p>Grab the best tasty burger of the town we make the best</p>
+                    <p> burger for you</p>
                     <button>Learn more</button>
                 </div>
                 <div id='hero-div'>
                     <img id='hero-img' src="/images/00.png" alt="chicken sandwich with lettuce tomatoe onions cheese pickles and ketchup" />
                 </div>
             </div >
-
-            <div id='miniMenu'>
-                <div className='menuCard'>
-                    <div className='circle one'>
-                        <img src='/images/kisspng-doner-kebab-kapsalon-pizza-turkish-cuisine-kebab-5ac9869a962f36.7779223015231566346152.png' alt='sandwich with roast beef lettuce cheese tomatoes sour kraut and bacon' />
+            <div id='container1'>
+                <div id='miniMenu'>
+                    <div className='menuCard'>
+                        <div className='circle one'>
+                            <img src='/images/kisspng-doner-kebab-kapsalon-pizza-turkish-cuisine-kebab-5ac9869a962f36.7779223015231566346152.png' alt='sandwich with roast beef lettuce cheese tomatoes sour kraut and bacon' />
+                        </div>
+                        <div className='price'>
+                            <h5>Ham Burger</h5>
+                            <p><strong>$10.30</strong></p>
+                        </div>
                     </div>
-                    <div>
-                        <h5>Ham Burger</h5>
-                        <p>$10.30</p>
+                    <div className='menuCard'>
+                        <div className='circle two'>
+                            <img src='/images/kisspng-submarine-sandwich-barbecue-chicken-firehouse-subs-sub-sandwich-5b165469257050.2481338715281900571534.png'
+                                alt='firehouse sub sandwich' />
+                        </div>
+                        <div className='price'>
+                            <h5>Sub Sandwich</h5>
+                            <p><strong>$10.30</strong></p>
+                        </div>
+                    </div>
+                    <div className='menuCard'>
+                        <div className='circle'>
+                            <img src='/images/kisspng-chicago-style-hot-dog-hamburger-barbecue-bxe1nh-mx-hot-dog-5a735972beccb3.3331627015175089787815.png' alt='hot dog with mustard relish and pickles' />
+                        </div>
+                        <div className='price'>
+                            <h5>Hot Dog</h5>
+                            <p><strong>$10.30</strong></p>
+                        </div>
                     </div>
                 </div>
-                <div className='menuCard'>
-                    <div className='circle two'>
-                        <img src='/images/kisspng-submarine-sandwich-barbecue-chicken-firehouse-subs-sub-sandwich-5b165469257050.2481338715281900571534.png'
-                            alt='firehouse sub sandwich' />
-                    </div>
-                    <div>
-                        <h5>Sub Sandwich</h5>
-                        <p>$10.30</p>
-                    </div>
+                <div id='fries'>
+                    <img src="\images\kisspng-french-fries-deep-frying-junk-food-french-fries-5a81fb02577680.2267572515184678423583.png" alt="french fries" />
                 </div>
-                <div className='menuCard'>
-                    <div className='circle'>
-                        <img src='/images/kisspng-chicago-style-hot-dog-hamburger-barbecue-bxe1nh-mx-hot-dog-5a735972beccb3.3331627015175089787815.png' alt='hot dog with mustard relish and pickles' />
-                    </div>
-                    <div>
-                        <h5>Hot Dog</h5>
-                        <p>$10.30</p>
-                    </div>
-                </div>
-            </div>
-            <div id='fries'>
-                <img src="\images\kisspng-french-fries-deep-frying-junk-food-french-fries-5a81fb02577680.2267572515184678423583.png" alt="french fries" />
             </div>
         </header >
     )
